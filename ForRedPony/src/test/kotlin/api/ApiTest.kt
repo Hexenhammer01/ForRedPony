cd2372c656a5970712e34ccae1f386582ac9b7d1
Hexenhammer01 <railxpp@gmail.com>
2025-01-25 18:43:49 +0300

    first test

--- ForRedPony/src/test/kotlin/api/ApiTest.kt
+++ ForRedPony/src/test/kotlin/api/ApiTest.kt
@@ -9,16 +9,12 @@ class ApiTest {
     @Test
     fun `validate GET endpoint`() {
         RestAssured.given()
-            .baseUri("https://jsonplaceholder.typicode.com")
+            .baseUri("https://petstore.swagger.io")
             .log().all() // Логирование всех параметров запроса
             .`when`()
-            .get("/todos/1")
+            .get("/v2/pet/findByStatus?status=available")
             .then()
             .log().all() // Логирование всех параметров ответа
             .statusCode(200)
-            .body("userId", equalTo(1))
-            .body("id", equalTo(1))
-            .body("title", equalTo("delectus aut autem"))
-            .body("completed", equalTo(false))
     }
 }
